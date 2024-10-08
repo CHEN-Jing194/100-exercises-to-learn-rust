f284856f78d6cd0f6f3fab8bc059d9267967b469
leo <chenjing194@gmail.com>
2024-10-09 00:24:55 +0800

    Refactor dependencies in Cargo.toml files

--- exercises/05_ticket_v2/15_outro/src/description.rs
+++ exercises/05_ticket_v2/15_outro/src/description.rs
@@ -1,9 +1,41 @@
 // TODO: Implement `TryFrom<String>` and `TryFrom<&str>` for the `TicketDescription` type,
 //   enforcing that the description is not empty and is not longer than 500 bytes.
 //   Implement the traits required to make the tests pass too.
-
+#[derive(Debug, PartialEq, Clone, thiserror::Error)]
+pub enum DescriptionError {
+    #[error("The description cannot be empty")]
+    Empty,
+    #[error("The description cannot be longer than 500 bytes")]
+    TooLong,
+}
+#[derive(Debug, PartialEq, Clone)]
 pub struct TicketDescription(String);
 
+impl TryFrom<String> for TicketDescription {
+    type Error = DescriptionError;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        validate(value.as_str())?;
+        Ok(TicketDescription(value))
+    }
+}
+
+impl TryFrom<&str> for TicketDescription {
+    type Error = DescriptionError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        validate(value)?;
+        Ok(TicketDescription(value.to_string()))
+    }
+}
+
+fn validate(value: &str) -> Result<(), DescriptionError> {
+    if value.is_empty() {
+        Err(DescriptionError::Empty)
+    } else if value.len() > 500 {
+        Err(DescriptionError::TooLong)
+    } else {
+        Ok(())
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
